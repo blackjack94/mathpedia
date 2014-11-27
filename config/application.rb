ef921dcfb36d893520a064fae12af332585e1f20
blackjack94 <letadangkhoa@gmail.com>
2014-11-28 04:31:58 +0700

    Ruby 2.1.5 with GC Profiler

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module Mathpedia
     #ADDED BY BLACKJACK94
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     ENV['RAILS_CACHE_ID'] = `git log --pretty=format:%h -n1`.strip
+    GC::Profiler.enable
   end
 end
